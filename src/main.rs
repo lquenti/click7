d752412b041e17a8ee15cbdd79a3681bb5a98e66
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-02-18 22:08:28 +0100

    get a static image everytime

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,11 @@
 *
 * ## REST API
 * - [x] Find out a Rust framework to use
-* - [ ] Get a health check API endpoint returning 200
+* - [x] Get a health check API endpoint returning 200
 * - [ ] Find out how to return an Image
 * - [ ] Create a debug endpoint where one can query any number and get it
 * - [ ] Create a `/counter/<ID>` endpoint that always returns the same number
-* 
+*
 * ## sqlite
 * todo
  */
@@ -31,10 +31,30 @@
 mod cli;
 mod img_gen;
 
-use axum::{Router, routing};
+use axum::{
+    http::{header::CONTENT_TYPE, Response, StatusCode},
+    routing, Router,
+};
 use clap::Parser;
+use image::ImageEncoder;
+use img_gen::generate_image;
 
 use crate::cli::Args;
+
+async fn debug() -> (Response<()>, Vec<u8>) {
+    let img = generate_image(1337, 7, 20, 20);
+    let mut bytes: Vec<u8> = vec![];
+
+    image::codecs::png::PngEncoder::new(&mut bytes)
+        .write_image(&img, img.width(), img.height(), image::ColorType::Rgba8)
+        .unwrap();
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(CONTENT_TYPE, "image/png")
+        .body(())
+        .unwrap();
+    (response, bytes)
+}
 
 #[tokio::main]
 async fn main() {
@@ -47,10 +67,12 @@ async fn main() {
         panic!("Not all images have the same height!");
     }
 
-    /* Create route */
+    /* Define routes */
     let app = Router::new()
-        .route("/health_check", routing::get(|| async {"Ok"}));
+        .route("/health_check", routing::get(|| async { "Ok" }))
+        .route("/debug", routing::get(debug));
 
+    /* start server */
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
